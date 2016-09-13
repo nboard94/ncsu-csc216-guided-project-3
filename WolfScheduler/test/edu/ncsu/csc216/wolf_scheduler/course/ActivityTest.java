7b5aab432b9e9228e789a054436ab2629c2e574b
NBoar <NBoar@DESKTOP-U79JFG9>
2016-09-13 18:38:54 -0400

    compareToTest is now more specific

--- WolfScheduler/test/edu/ncsu/csc216/wolf_scheduler/course/ActivityTest.java
+++ WolfScheduler/test/edu/ncsu/csc216/wolf_scheduler/course/ActivityTest.java
@@ -17,6 +17,13 @@ public class ActivityTest {
 	    } catch (ConflictException e) {
 	        fail("A ConflictException was thrown when two Activities at the same time on completely distinct days were compared.");
 	    }
+	    try {
+	        a2.checkConflict(a1);
+	        assertEquals("Incorrect meeting string for this Activity.", "MW 1:30PM-2:45PM", a1.getMeetingString());
+	        assertEquals("Incorrect meeting string for possibleConflictingActivity.", "TH 1:30PM-2:45PM", a2.getMeetingString());
+	    } catch (ConflictException e) {
+	        fail("A ConflictException was thrown when two Activities at the same time on completely distinct days were compared.");
+	    }
 	    
 	  //Update a1 with the same meeting days and a start time that overlaps the end time of a2
 	    a1.setMeetingDays("TH");
@@ -27,6 +34,39 @@ public class ActivityTest {
 	    } catch (ConflictException e) {
 	        //Check that the internal state didn't change during method call.
 	        assertEquals("TH 2:45PM-3:30PM", a1.getMeetingString());
+	        assertEquals("TH 1:30PM-2:45PM", a2.getMeetingString());
+	    }
+	    
+	  //Update a1 with a start time before the end time of a2, and an end time after the end time of a2
+	    a1.setActivityTime(1440, 1530);
+	    try {
+	        a1.checkConflict(a2);
+	        fail(); //ConflictException should have been thrown, but was not.
+	    } catch (ConflictException e) {
+	        //Check that the internal state didn't change during method call.
+	        assertEquals("TH 2:40PM-3:30PM", a1.getMeetingString());
+	        assertEquals("TH 1:30PM-2:45PM", a2.getMeetingString());
+	    }
+	    
+		//Update a1 with a start time and end time exactly the same as a2
+	    a1.setActivityTime(1330, 1445);
+	    try {
+	        a1.checkConflict(a2);
+	        fail(); //ConflictException should have been thrown, but was not.
+	    } catch (ConflictException e) {
+	        //Check that the internal state didn't change during method call.
+	        assertEquals("TH 1:30PM-2:45PM", a1.getMeetingString());
+	        assertEquals("TH 1:30PM-2:45PM", a2.getMeetingString());
+	    }
+	    
+	  //Update a1 with a start time before the end time of a2, and an end time after the start time of a2
+	    a1.setActivityTime(1230, 1440);
+	    try {
+	        a1.checkConflict(a2);
+	        fail(); //ConflictException should have been thrown, but was not.
+	    } catch (ConflictException e) {
+	        //Check that the internal state didn't change during method call.
+	        assertEquals("TH 12:30PM-2:40PM", a1.getMeetingString());
 	        assertEquals("TH 1:30PM-2:45PM", a2.getMeetingString());
 	    }
 	}
