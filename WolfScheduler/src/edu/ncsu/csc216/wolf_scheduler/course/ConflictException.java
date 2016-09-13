5a4d697098753b352009606646a84c9026df6398
NBoar <NBoar@DESKTOP-U79JFG9>
2016-09-13 13:28:01 -0400

    Improved functionality of ConflictException and updated ConflictExceptionTest

--- WolfScheduler/src/edu/ncsu/csc216/wolf_scheduler/course/ConflictException.java
+++ WolfScheduler/src/edu/ncsu/csc216/wolf_scheduler/course/ConflictException.java
@@ -2,4 +2,14 @@ package edu.ncsu.csc216.wolf_scheduler.course;
 
 public class ConflictException extends Exception {
 
+	/** ID used for serialization. */
+	private static final long serialVersionUID = 1L;
+
+	ConflictException (String s) {
+		super(s);
+	}
+	
+	ConflictException() {
+		super("Schedule conflict.");
+	}
 }
